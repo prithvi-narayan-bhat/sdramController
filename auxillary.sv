677e448f734d2727106153378301ca3a02314493
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-07-30 11:31:58 -0500

    Added task for delays

--- auxillary.sv
+++ auxillary.sv
@@ -1,29 +1,16 @@
-// function logic waitTime (
-//         input clk,              // 100MHz Clock
-//         input waitTime          // Return value
-//     );
+task delayNanoseconds (
+        input clk,              // 100MHz Clock
+        input delayTimeNs,      // Return value
+        output logic timeout    // Return value
+    );
 
-//     int i;
-//     if (waitTime == 3'd0)
-//     begin
-//         for (i = 0; i < 10000; i++)
-//             @(posedge clk);
-//         if (i == 10000) waitTime = 1'b1;
-//         else            waitTime = 1'b0;
-//     end
+    int i;
+    timeout = 0;
+    for (i = 0; i < (delayTimeNs); i++)
+        @(posedge clk);         // Wait for a positive edge of the clock
 
-//     else if (waitTime == 3'd1)
-//     begin
-//         for (i = 0; i < 1600; i++)
-//             @(posedge clk);
-//         if (i == 1600)  waitTime = 1'b1;
-//         else            waitTime = 1'b0;
-//     end
+    timeout = 1;
 
-//     else if (waitTime == 3'd2)
-//     begin
-//         #6
-//         waitTime = 1'b1;
-//     end
+    if (timeout)    return;     // Indicate delay has been effected
 
-// endfunction
+endtask
--- synth_1/runme.sh
+++ synth_1/runme.sh
@@ -20,7 +20,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='/home/bhat/Documents/Academics/Semester-III/CSE-5358/SDRAM_Controller/sdramController.runs/synth_1'
+HD_PWD='/home/bhat/Documents/Academics/Semester-III/CSE-5358/sdramController/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
