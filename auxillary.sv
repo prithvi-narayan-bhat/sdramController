1302c38386bdbf4d1668861bac2843eeac029df3
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-08-05 17:21:38 -0500

    Cleaned delay logic

--- auxillary.sv
+++ auxillary.sv
@@ -1,14 +1,18 @@
 task delayNanoseconds (
         input delayTimeNs,      // Return value
+        input RESETn,
         output logic timeout    // Return value
     );
 
     int i = delayTimeNs;
     timeout = 0;                // Clear timeout
     while ( i > 0)
+    begin
         i--;                    // Decrement
+        if (!RESETn)    break;
+    end
 
-    timeout = 1;                // Set timeout
+    if (i == 0) timeout = 1;                // Set timeout
 
     if (timeout)    return;     // Indicate delay has been effected
 
